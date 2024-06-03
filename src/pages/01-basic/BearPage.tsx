f5c0fee2c094689a27db580136902e5265285881
Cristian Hernandez <cristian5464@outlook.com>
2024-06-03 17:14:53 -0600

    fin seccion 2

--- src/pages/01-basic/BearPage.tsx
+++ src/pages/01-basic/BearPage.tsx
@@ -1,6 +1,9 @@
+import { useShallow } from 'zustand/react/shallow';
 import { WhiteCard } from '../../components';
+import { useBearStore } from '../../stores';
 
 export const BearPage = () => {
+
   return (
     <>
       <h1>Contador de Osos</h1>
@@ -9,41 +12,13 @@ export const BearPage = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
 
-        <WhiteCard centered>
-          <h2>Osos Negros</h2>
+        <BlackBears />
 
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
+        <PolarBears />
 
-        </WhiteCard>
+        <PandaBears />
 
-        <WhiteCard centered>
-          <h2>Osos Polares</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-
-          </div>
-
-        </WhiteCard>
-
-        <WhiteCard centered>
-          <h2>Osos Pandas</h2>
-
-          <div className="flex flex-col md:flex-row">
-            <button> +1</button>
-            <span className="text-3xl mx-2 lg:mx-10"> 0 </span>
-            <button>-1</button>
-          </div>
-
-        </WhiteCard>
-
-
+        <BearsDisplay />
 
 
       </div>
@@ -51,3 +26,83 @@ export const BearPage = () => {
     </>
   );
 };
+
+
+
+export const BlackBears = () => {
+  const blackBears = useBearStore(state => state.blackBears);// Obtencion de propiedad blackBears del estado (state). Esta instruccion en este punto de este componente, se mantiene al pendiente de cualquier cambio que suceda en el valor de blackBears que es propiedad de state. En caso de que cambie el valor de dicha propiedad llamada blackBears de "state", hará que se vuelva a ejecutar el renderizado (return) de este componente (BlackBears)
+  const increaseBlackBears = useBearStore(state => state.increaseBlackBears);// Cambia el estado o valor de propiedad blackBears (que es propiedad de state donde state es de tipo BearState) al incremento en 1 de dicha propiedad blackBears
+  // const { blackBears, increaseBlackBears } = useBearStore(state => state);
+
+  return (
+    <WhiteCard centered>
+          <h2>Osos Negros</h2>
+
+          <div className="flex flex-col md:flex-row">
+            <button onClick={() => increaseBlackBears(1)}> +1</button>
+            <span className="text-3xl mx-2 lg:mx-10"> {blackBears} </span>
+            <button onClick={() => increaseBlackBears(-1)}>-1</button>
+          </div>
+
+        </WhiteCard>
+  );
+}
+
+export const PolarBears = () => {
+  const polarBears = useBearStore(state => state.polarBears);
+  const increasePolarBears = useBearStore(state => state.increasePolarBears);
+
+  return (
+    <WhiteCard centered>
+          <h2>Osos Polares</h2>
+
+          <div className="flex flex-col md:flex-row">
+            <button onClick={() => increasePolarBears(1)}> +1</button>
+            <span className="text-3xl mx-2 lg:mx-10"> {polarBears} </span>
+            <button onClick={() => increasePolarBears(-1)}>-1</button>
+
+          </div>
+
+        </WhiteCard>
+  );
+}
+
+export const PandaBears = () => {
+  const pandaBears = useBearStore(state => state.pandaBears);
+  const increasePandaBears = useBearStore(state => state.increasePandaBears);
+
+  return (
+    <WhiteCard centered>
+          <h2>Osos Pandas</h2>
+
+          <div className="flex flex-col md:flex-row">
+            <button onClick={() => increasePandaBears(1)}> +1</button>
+            <span className="text-3xl mx-2 lg:mx-10"> {pandaBears} </span>
+            <button onClick={() => increasePandaBears(-1)}>-1</button>
+          </div>
+
+        </WhiteCard>
+  );
+}
+
+
+export const BearsDisplay = () => {
+  // const bears = useBearStore(state => state.bears);
+  const bears = useBearStore(useShallow(state => state.bears));// useShallow se encargará de determinar si el nuevo estado que va a tomar objeto "bears" de "state" (state es de tipo BearState) es diferente a su estado anterior y, en caso de que asi sea, pues, re-rendizará componente en cuestion (componente BearsDisplay en este caso).
+  const doNothing = useBearStore(state => state.doNothing);
+  const addBear = useBearStore(state => state.addBear);
+  const clearBears = useBearStore(state => state.clearBears);// Cambia el estado o valor de objeto bears (que es propiedad de state) a un objeto vacio ([])
+
+  return (
+    <WhiteCard>
+      <h1>Osos</h1>
+      <button onClick={doNothing}>No hace nada</button>
+      <button className='mt-2' onClick={addBear}>Agregar oso</button>
+      <button className='mt-2' onClick={clearBears}>Borrar osos</button>
+
+      <pre>
+        {JSON.stringify(bears, null, 2)}
+      </pre>
+    </WhiteCard>
+  );
+}
---
+++ src/stores/bears/bears.store.ts
@@ -0,0 +1,58 @@
+import { create } from 'zustand'
+
+interface Bear {
+    id: number;
+    name: string;
+}
+
+interface BearState {
+    blackBears: number;
+    polarBears: number;
+    pandaBears: number;
+
+    bears: Bear[];
+
+    computed: {
+        totalBears: number
+    };// esta propiedad llamada computed luce como un objeto con la propiedad totalBears la cual es de tipo number
+    
+    increaseBlackBears: (by: number) => void;
+    increasePolarBears: (by: number) => void;
+    increasePandaBears: (by: number) => void;
+
+    doNothing: () => void;
+    addBear: () => void;
+    clearBears: () => void;
+
+    
+    // increasePopulation: () => void;
+    // removeAllBears: () => void;
+    // updateBears: (newBears:number) => void;// updateBears será una funcion la cual no retorna nada (retorna void) y recibe un argumento de tipo number
+}
+
+export const useBearStore = create<BearState>()((set, get) => ({
+    blackBears: 10,
+    polarBears: 5,
+    pandaBears: 1,
+
+    bears: [{ id: 1, name: 'Oso #1' }],// bears es un Objeto anidado del store useBearStore
+
+    computed: {
+        get totalBears() {
+            return get().blackBears + get().polarBears + get().pandaBears + get().bears.length;
+        }
+    },
+
+    increaseBlackBears: (by: number) => set(state => ({ blackBears: state.blackBears + by })),// set(state => ({ blackBears: state.blackBears + by })) implicitamente o se entiende que significa: { ...state, blackBears: state.blackBears + by }, donde state hace referencia a un objeto tipo BearState
+    increasePolarBears: (by: number) => set(state => ({ polarBears: state.polarBears + by })),
+    increasePandaBears: (by: number) => set(state => ({ pandaBears: state.pandaBears + by })),
+
+    // Metodos para el objeto anidado bears (bears es un objeto o arreglo. Un arreglo es un objeto)
+    doNothing: () => set(state => ({ bears: [...state.bears] })),// Para Zustand, el spread de un objeto anidado (para vaciarse en un arreglo en este caso) considera que es un nuevo cambio de estado. Usar useShallow para ayudar a Zustand a determinar que el valor del objeto anidado (state.bears) realmente es diferente al nuevo valor ([...state.bears]) y en caso de que sí sean difentes, pues cambia el estado al de state.bears a su nuevo valor provocando que se renderice el componente donde se está usando "bears" de state... En resumen, usar useShallow cuando sea necesario para objetos anidados (como bears)
+    addBear: () => set(state => ({
+        bears: [
+            ...state.bears,
+            { id: state.bears.length + 1, name: `Oso #${state.bears.length + 1}` }
+    ] })),
+    clearBears: () => set({ bears: [] }),// Dado que no se está usando "state" para establecer un nuevo estado basado en dicho "state", lo quitamos a dicho "state"
+}))
